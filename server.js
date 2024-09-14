9e7ce98f2ca5aa3019c0b1f44f9b02dcddfe0859
kouhei <kouhei.basketball7.8@gmail.com>
2024-09-14 11:48:30 +0900

    add:authやpostのルーティング設定

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const express = require("express");
 // インスタンス化
 const app = express();
 const userRoute = require('./routes/users')
+const authRoute = require('./routes/auth')
+const postRoute = require('./routes/posts')
 // port指定
 const PORT = 3306;
 
 // ミドルウェア
 // users.jsで設定したルートは下記をデフォルトルートとして使う
 app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/posts', postRoute);
 
 // api作成
 app.get("/", (req, res) => {
