ae0974c3863d7ea8a0b8ce773183552537721438
kouhei <kouhei.basketball7.8@gmail.com>
2024-09-14 10:27:44 +0900

    add:ローカルサーバーの立ち上げまで確認

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log('node.js');
+// requireでexpress呼び出す
+const express = require("express");
+// インスタンス化
+const app = express();
+// port指定
+const PORT = 3306;
+
+// api作成
+app.get("/", (req, res) => {
+    res.send('hello world')
+})
+app.get("/users", (req, res) => {
+    res.send('ユーザーです')
+})
+
+app.listen(PORT, () => console.log("listenでサーバー起動する"))
