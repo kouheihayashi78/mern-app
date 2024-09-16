5f5f44eb1008a46ff2434cc1d5137c09f8e7348b
kouhei <kouhei.basketball7.8@gmail.com>
2024-09-16 15:02:16 +0900

    delete:不要な記述削除

--- routes/users.js
+++ routes/users.js
@@ -5,9 +5,5 @@ router.get('/', (req, res) => {
     res.send('userRouter');
 });
 
-router.get('/profile', (req, res) => {
-    res.send('profileです');
-});
-
 // jsにおいて、ファイルに存在する変数や関数を別のファイルで実行する（改めて言語化）
 module.exports = router;
