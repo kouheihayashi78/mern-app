c18f1f7538860b0511dd16b546fb8bf87976d061
kouhei <kouhei.basketball7.8@gmail.com>
2024-10-26 17:47:16 +0900

    add:特定ユーザー取得機能実装

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,21 @@ router.get('/', (req, res) => {
     res.send('userRouter');
 });
 
+// ユーザーの取得
+router.get('/:id', async (req, res) => {    
+    try {
+        const user = await User.findById(req.params.id);
+        // 分割代入でパスワードと更新日時を表示させないようにする
+        // スプレッド構文で上記二つ以外のものを格納してそれらをjson形式で渡す
+        // _docはMongooseでRaw値を格納するプロパティ
+        const { password, updatedAt, ...other } = user._doc;
+        res.status(200).json(other);
+    } catch (error) {
+        return res.status(500).json(error)
+    }
+
+})
+
 // ユーザーの更新
 // /:idとすることで、ランダムな文字列に対応できるということ
 // mongodbだと、ObjectId（ランダムな文字列）でユーザーを判別する
@@ -35,7 +50,6 @@ router.delete('/:id', async (req, res) => {
     if(req.body.userId === req.params.id || req.body.isAdmin) {
         try {
             const user = await User.findByIdAndDelete(req.params.id);
-
             res.status(200).json('ユーザー情報が削除されました。');
         } catch (error) {
             return res.status(500).json(error)
@@ -45,6 +59,7 @@ router.delete('/:id', async (req, res) => {
         return res.status(403).send('自分のアカウントの時に情報を削除できます。')
     }
 })
+
 
 // jsにおいて、ファイルに存在する変数や関数を別のファイルで実行する（改めて言語化）
 module.exports = router;
