c70ccf60afc45294a3696819797fbefd85acf4b3
kouhei <kouhei.basketball7.8@gmail.com>
2024-10-27 11:45:57 +0900

    add:投稿作成のAPI実装

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
+const Post = require('../models/Post');
 
 router.get('/', (req, res) => {
     res.send('postRouter');
 });
+
+// 投稿を作成する
+router.post('/', async(req,res) => {
+    // インスタンス化
+    const newPost = new Post(req.body);
+    try {
+        const savedPost = await newPost.save();
+        return res.status(200).json(savedPost);
+    } catch (error) {
+        // 文字数オーバーや型のエラーなどでここを通る
+        return res.status.json(error);
+    }
+})
 
 module.exports = router;
