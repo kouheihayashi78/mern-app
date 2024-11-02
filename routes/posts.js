50c0b65214218e450f26be843a0384cb123fff4f
kouhei <kouhei.basketball7.8@gmail.com>
2024-11-02 16:19:52 +0900

    add:投稿削除のAPI実装

--- routes/posts.js
+++ routes/posts.js
@@ -37,4 +37,19 @@ router.put('/:id', async (req, res) => {
     }
 })
 
+// 投稿を削除する
+router.delete('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.userId === req.body.userId) {
+            await post.deleteOne();
+            return res.status(200).json('投稿を削除しました。')
+        } else {
+            return res.status(403).json('投稿者本人しか削除できません。');
+        }
+    } catch (error) {
+        return res.status(403).json(error);
+    }
+})
+
 module.exports = router;
