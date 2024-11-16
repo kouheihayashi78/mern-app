56b2e52b3a233b3e03f019f751a67c03aa90b9ec
kouhei <kouhei.basketball7.8@gmail.com>
2024-11-16 20:48:31 +0900

    fix:トップバーのクラス名とインデント修正

--- frontend/src/components/topbar/Topbar.tsx
+++ frontend/src/components/topbar/Topbar.tsx
@@ -1,31 +1,36 @@
 import React from "react";
-import Search from '@mui/icons-material/Search';
-import ChatIcon from '@mui/icons-material/Chat';
-import NotificationsIcon from '@mui/icons-material/Notifications';
+import Search from "@mui/icons-material/Search";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import "../topbar/Topbar.css";
 
 export const Topbar = () => {
   return (
     <div className="topbar-container">
       <div className="topbar-left">
-        <span className="logo">
-          mern-app
-        </span>
-        <div className="topbar-center">
-          <div className="search-bar">
-            <Search className="search-icon" />
-            <input type="text" className="search-input" placeholder="調べたいことを入力してください" />
-          </div>
+        <span className="logo">mern-app</span>
+      </div>
+      <div className="topbar-center">
+        <div className="search-bar">
+          <Search className="search-icon" />
+          <input
+            type="text"
+            className="search-input"
+            placeholder="調べたいことを入力してください"
+          />
         </div>
-        <div className="topbar-right">
-          <div className="btn-area">
+      </div>
+      <div className="topbar-right">
+        <div className="topbar-icons">
+          <div className="topbar-icon-item">
             <ChatIcon />
+            <span className="topbar-icon-badge">2</span>
           </div>
-          <div className="btn-area">
+          <div className="topbar-icon-item">
             <NotificationsIcon />
+            <span className="topbar-icon-badge">3</span>
           </div>
-        </div>
-        <div className="icon-area">
-            <img src="assets/person/1.jpeg" alt="1" />
+          <img src="assets/person/1.jpeg" alt="1" className="topbar-img" />
         </div>
       </div>
     </div>
