ee23e3ed1d5fb373fecf3177d8ebc9ca2c8bd855
kouhei <kouhei.basketball7.8@gmail.com>
2024-11-17 11:31:11 +0900

    add:サイドバー、タイムライン、ライトバーのコンポーネントを準備

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import { Topbar } from '../../components/topbar/Topbar.tsx'
+import React from 'react';
+import { Topbar } from '../../components/topbar/Topbar.tsx';
+import { Sidebar } from '../../components/sidebar/Sidebar.tsx';
+import { TimeLine } from '../../components/timeline/TimeLine.tsx';
+import { Rightbar } from '../../components/rightbar/Rightbar.tsx';
 
 export default function Home () {
   return (
     <>
       <Topbar />
+      <Sidebar />
+      <TimeLine />
+      <Rightbar />
     </>
   )
 }
