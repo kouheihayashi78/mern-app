ef493d347a97498192a1023c147c137f6e70043c
kouhei <kouhei.basketball7.8@gmail.com>
2024-11-28 21:59:39 +0900

    fix:微修正とshareコンポーネント表示

--- frontend/src/components/timeline/TimeLine.tsx
+++ frontend/src/components/timeline/TimeLine.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import './TimeLine.css'
+import Share from '../share/Share.tsx';
 
 export const TimeLine = () => {
   return (
     <div className="timeline">
       {/* shareコンポーネント */}
       <div className="timelineWrapper">
-        {/* <Share /> */}
+        <Share />
         {/* <Post /> */}
       </div>
     </div>
