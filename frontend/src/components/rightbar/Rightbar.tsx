9c6f03a9eca864d6f8c7b27d097a052b4d04a64d
kouhei <kouhei.basketball7.8@gmail.com>
2024-12-02 17:19:16 +0900

    add:rightコンポーネントのユーザー情報を表示する箇所を実装

--- frontend/src/components/rightbar/Rightbar.tsx
+++ frontend/src/components/rightbar/Rightbar.tsx
@@ -49,9 +49,68 @@ export function Rightbar() {
     );
   };
 
+  const ProfileRightbar = () => {
+    return (
+      <>
+        <h4 className="rightbarTitle">ユーザー情報</h4>
+        <div className="rightbarInfo">
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">出身:</span>
+            <span className="rightbarInfoKey"></span>
+          </div>
+          <h4 className="rightbarTitle">あなたの友達</h4>
+          <div className="rightbarFollowings">
+            <div className="rightbarFollowing">
+              <img
+                src="./assets/person/1.jpeg"
+                alt=""
+                className="rightbarFollowingImg"
+              />
+              <span className="rightbarFollowingName">ShinCode</span>
+            </div>
+            <div className="rightbarFollowing">
+              <img
+                src="./assets/person/2.jpeg"
+                alt=""
+                className="rightbarFollowingImg"
+              />
+              <span className="rightbarFollowingName">Yamaki</span>
+            </div>
+            <div className="rightbarFollowing">
+              <img
+                src="./assets/person/3.jpeg"
+                alt=""
+                className="rightbarFollowingImg"
+              />
+              <span className="rightbarFollowingName">Koga</span>
+            </div>
+            <div className="rightbarFollowing">
+              <img
+                src="./assets/person/4.jpeg"
+                alt=""
+                className="rightbarFollowingImg"
+              />
+              <span className="rightbarFollowingName">Matukubo</span>
+            </div>
+            <div className="rightbarFollowing">
+              <img
+                src="./assets/person/5.jpeg"
+                alt=""
+                className="rightbarFollowingImg"
+              />
+              <span className="rightbarFollowingName">Kikukawa</span>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
+
   return (
     <div className="rightbar">
       <div className="rightbarWrapper">
+        {/* todo: 今後userタイプによって下記の表示を切り替える */}
+        {<ProfileRightbar />}
         {<HomeRightbar />}
       </div>
     </div>
