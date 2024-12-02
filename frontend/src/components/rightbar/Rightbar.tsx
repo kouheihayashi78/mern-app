7770c50e59875d8c5ac1b074d09776b3ce3dc3c1
kouhei <kouhei.basketball7.8@gmail.com>
2024-12-02 17:15:32 +0900

    add:rightコンポーネントの雛形実装

--- frontend/src/components/rightbar/Rightbar.tsx
+++ frontend/src/components/rightbar/Rightbar.tsx
@@ -1,8 +1,59 @@
 import React from "react";
-import './Rightbar.css';
+import "./Rightbar.css";
 
-export const Rightbar = () => {
+export function Rightbar() {
+  const HomeRightbar = () => {
+    return (
+      <>
+        <div className="birthdayContainer">
+          <img src="assets/star.png" alt="" className="birthdayImg" />
+          <span className="birthdayText">
+            <b>フォロワー限定</b>イベント開催中！
+          </span>
+        </div>
+        <img className="rightbarAd" src="assets/ad.jpeg" alt="" />
+        <h4 className="rightbarTitle">オンラインの友達</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src="assets/person/3.jpeg"
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Shin Code</span>
+          </li>
+        </ul>
+        <p className="promotionTitle">プロモーション広告</p>
+        <img
+          className="rightbarPromotionImg"
+          src="assets/promotion/promotion1.jpeg"
+          alt=""
+        />
+        <p className="promotionName">ショッピング</p>
+        <img
+          className="rightbarPromotionImg"
+          src="assets/promotion/promotion2.jpeg"
+          alt=""
+        />
+        <p className="promotionName">カーショップ</p>
+        <img
+          className="rightbarPromotionImg"
+          src="assets/promotion/promotion3.jpeg"
+          alt=""
+        />
+        <p className="promotionName">ShinCode株式会社</p>
+      </>
+    );
+  };
+
   return (
-    <div className='rightbar'>Rightbar</div>
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        {<HomeRightbar />}
+      </div>
+    </div>
   );
-};
+}
