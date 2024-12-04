72af1d10b88b7337ab54f9bf3a1bd2a8570a5e78
kouhei <kouhei.basketball7.8@gmail.com>
2024-12-04 16:17:59 +0900

    add:ダミーデータを表示するように実装

--- frontend/src/components/post/Post.tsx
+++ frontend/src/components/post/Post.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import "./Post.css";
 import { MoreVert } from "@mui/icons-material";
+import { Users } from "../../dummyData.js";
 
 function Post({ post }) {
+  const userFilter = (postId) => {
+    const user = Users.filter((user) => user.id === postId);
+    return user[0].username;
+  }
+  const imgFilter = (postId) => {
+    const user = Users.filter((user) => user.id === postId);
+    return user[0].profilePicture;
+  }
   return (
     <div className="post">
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
-            <img src="./assets/person/1.jpeg" alt="" className="postProfileImg"/>
-            <span className="postUsername">テスト 太郎</span>
+            <img src={imgFilter(post.id)} alt="" className="postProfileImg"/>
+            <span className="postUsername">{userFilter(post.id)}</span>
             <span className="postDate">{post.date}</span>
           </div>
           <div className="postTopRight">
