f1697f04ca8b79eb75c35f80c6bf33e6ae73d5fc
kouhei <kouhei.basketball7.8@gmail.com>
2024-12-08 12:41:15 +0900

    add:ルーティング実装

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home.tsx";
+import Login from "./pages/login/Login.tsx";
+import Profile from "./pages/profile/Profile.tsx";
+import Register from "./pages/register/Register.tsx";
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
+        {/* :usernameで任意の文字列 */}
+        <Route exact path="/profile/:username" element={<Profile />} /> 
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
