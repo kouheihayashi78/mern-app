be7c55a1879b2e36b103a611363ae5e794313f4e
kouhei <kouhei.basketball7.8@gmail.com>
2024-12-14 17:55:27 +0900

    fix:ダミーデータを表示している投稿情報をapiから取得して表示するように修正

--- frontend/src/components/post/Post.tsx
+++ frontend/src/components/post/Post.tsx
@@ -1,21 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Post.css";
 import { MoreVert } from "@mui/icons-material";
-import { Users } from "../../dummyData.js";
+import axios from "axios";
 
 function Post({ post }) {
   const [like, setLike] = useState(post.like);
   const [isLiked, setIsLiked] = useState(false);
   const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
+  const [ user, setUser ] = useState({});
 
-  const userFilter = (postId) => {
-    const user = Users.filter((user) => user.id === postId);
-    return user[0].username;
-  }
-  const imgFilter = (postId) => {
-    const user = Users.filter((user) => user.id === postId);
-    return PUBLIC_FOLDER + user[0].profilePicture;
-  }
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await axios.get(`/users/${post.userId}`);
+        console.log(res.data)
+        setUser(res.data);
+      } catch (e) {
+        console.error(e)
+      }
+    }
+    fetchUser();
+  }, []);
+
   const handleLike = () => {
     setLike(isLiked ? like - 1 : like + 1);
     setIsLiked(!isLiked);
@@ -25,8 +31,11 @@ function Post({ post }) {
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
-            <img src={imgFilter(post.id)} alt="" className="postProfileImg"/>
-            <span className="postUsername">{userFilter(post.id)}</span>
+            <img src={user.profilePicture ? user.profilePicture : `${PUBLIC_FOLDER}/person/noAvatar.png`} 
+              alt="" 
+              className="postProfileImg"
+            />
+            <span className="postUsername">{user.username}</span>
             <span className="postDate">{post.date}</span>
           </div>
           <div className="postTopRight">
