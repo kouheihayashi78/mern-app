3e172de432a3ffae552513cd60f3f4ed673026ce
kouhei <kouhei.basketball7.8@gmail.com>
2024-12-14 17:54:59 +0900

    fix:ダミーデータを表示しているタイムライン部分をapiから取得して表示するように修正

--- frontend/src/components/timeline/TimeLine.tsx
+++ frontend/src/components/timeline/TimeLine.tsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './TimeLine.css'
 import Share from '../share/Share.tsx';
 import Post from "../post/Post.tsx";
-import { Posts } from "../../dummyData.js";
+import axios from "axios";
 
 export const TimeLine = () => {
+  const [ posts, setPosts ] = useState<string[]>([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await axios.get('/posts/timeline/671ca9012a8510b37cd72c4f');
+        console.log(res.data)
+        setPosts(res.data);
+      } catch (e) {
+        console.error(e.response)
+      }
+    }
+    fetchPosts();
+  }, []);
+  
   return (
     <div className="timeline">
       {/* shareコンポーネント */}
       <div className="timelineWrapper">
         <Share />
-        {Posts.map((post) => (
+        {posts.map((post) => (
           <Post post={post} key={post.id} />
         ))}
       </div>
